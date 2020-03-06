a8ad1d1127aa10a7208d1fbbcd90fb381db1cc5c
Tanuja <tanuja6.k@gmail.com>
2020-03-06 15:11:35 +0530

    To-Do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ToDo-app';
-}
+// Now we have to store the value captured from the input. We can create an empty array in app.component.ts inside the AppComponent class:
+export class AppComponent { todoArray=[] 
+  addTodo(value: any){    
+    this.todoArray.push(value) 
+       console.log(this.todos) 
+       }
+       deleteItem(todo: any){  
+          for(let i=0 ;i<= this.todoArray.length ;i++)
+          {    
+            if(todo== this.todoArray[i])
+            {     
+              this.todoArray.splice(i,1)    
+            }   
+          }  
+        }
+  todos(todos: any) {
+    throw new Error("Method not implemented.");
+  }
+     }
